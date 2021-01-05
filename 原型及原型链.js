9110c15099d3b51a035f33f5209a98aac1eff902
Filish .K <filishope@163.com>
2021-01-05 15:36:38 +0800

    position 瀑布流(有异步问题)

--- 原型及原型链.js
+++ 原型及原型链.js
@@ -1,17 +1,20 @@
-function Animal(name,action) {
+function Animal(name, action) {
     this.name = name;
     this.action = action;
 }
-Animal.prototype = {
-    eat(){
-        console.log(`${this.name} like ${this.action}`)
-    }
+// Animal.prototype = {
+//     eat(){
+//         console.log(`${this.name} like ${this.action}`)
+//     }
+// }
+Animal.prototype.eat = function () {
+    console.log(`${this.name} like ${this.action}`)
 }
-const dog = new Animal('dog','eat');
+const dog = new Animal('dog', 'eat');
 dog.eat();
-console.log(dog.__proto__ === Animal.prototype) //true
-console.log(dog.constructor === Animal) //false
-console.log(Animal.prototype.constructor === Animal) //false
+console.log(dog.__proto__ === Animal.prototype) //true true
+console.log(dog.constructor === Animal) //false true
+console.log(Animal.prototype.constructor === Animal) //false true
 console.log('dog.__proto__', dog.__proto__) //eat: [Function: eat]
 console.log('Animal.prototype', Animal.prototype) //eat: [Function: eat]
 console.log('dog.constructor', dog.constructor) //function Object() { [native code] }
